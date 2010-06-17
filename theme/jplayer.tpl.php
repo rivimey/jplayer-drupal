3abfaffe24f25e202beb7ef4252c77fadf5394f7
Baej Owczarczyk <blazey@353861.no-reply.drupal.org>
2010-06-17 16:01:18 +0000

    patch #796860 by q0rban: adding titles in .tpl

--- theme/jplayer.tpl.php
+++ theme/jplayer.tpl.php
@@ -11,26 +11,26 @@
 <div class="jp-<?php print $mode; ?>-player">
   <div class="jp-interface">
     <ul class="jp-controls">
-      <li><a href="#" id="<?php print $player_id; ?>-play" class="jp-play"><?php print t('play'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-pause" class="jp-pause"><?php print t('pause'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-stop"class="jp-stop"><?php print t('stop'); ?></a></li>
+      <li><a href="#" id="<?php print $player_id; ?>-play" class="jp-play" title="<?php print t('play'); ?>"><?php print t('play'); ?></a></li>
+      <li><a href="#" id="<?php print $player_id; ?>-pause" class="jp-pause" title="<?php print t('pause'); ?>"><?php print t('pause'); ?></a></li>
+      <li><a href="#" id="<?php print $player_id; ?>-stop"class="jp-stop" title="<?php print t('stop'); ?>"><?php print t('stop'); ?></a></li>
 
-      <li><a href="#" id="<?php print $player_id; ?>-volume-min" class="jp-volume-min"><?php print t('min volume'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-volume-max" class="jp-volume-max"><?php print t('max volume'); ?></a></li>
+      <li><a href="#" id="<?php print $player_id; ?>-volume-min" class="jp-volume-min" title="<?php print t('min volume'); ?>"><?php print t('min volume'); ?></a></li>
+      <li><a href="#" id="<?php print $player_id; ?>-volume-max" class="jp-volume-max" title="<?php print t('max volume'); ?>"><?php print t('max volume'); ?></a></li>
       <?php if ($mode == 'playlist'): ?>
 
-      <li><a href="#" id="<?php print $player_id; ?>-volume-previous" class="jp-previous"><?php print t('previous'); ?></a></li>
-      <li><a href="#" id="<?php print $player_id; ?>-volume-next" class="jp-next"><?php print t('next'); ?></a></li>
+      <li><a href="#" id="<?php print $player_id; ?>-volume-previous" class="jp-previous" title="<?php print t('previous'); ?>"><?php print t('previous'); ?></a></li>
+      <li><a href="#" id="<?php print $player_id; ?>-volume-next" class="jp-next" title="<?php print t('next'); ?>"><?php print t('next'); ?></a></li>
       <?php endif; ?>
 
     </ul>
-    <div id="<?php print $player_id; ?>-progress" class="jp-progress">
+    <div id="<?php print $player_id; ?>-progress" class="jp-progress" title="<?php print t('progress'); ?>">
       <div id="<?php print $player_id; ?>-load-bar" class="jp-load-bar">
         <div id="<?php print $player_id; ?>-play-bar" class="jp-play-bar"></div>
       </div>
     </div>
 
-    <div id="<?php print $player_id; ?>-volume-bar" class="jp-volume-bar">
+    <div id="<?php print $player_id; ?>-volume-bar" class="jp-volume-bar" title="<?php print t('volume'); ?>">
       <div id="<?php print $player_id; ?>-volume-bar-value" class="jp-volume-bar-value"></div>
     </div>
     <div id="<?php print $player_id; ?>-play-time" class="jp-play-time"></div>
@@ -41,7 +41,7 @@
     <ul>
       <?php if ($mode == 'playlist'): ?>
         <?php foreach ($items as $number => $item): ?>
-          <li<?php print $item['class'] ? ' class="' . $item['class'] . '"' : '' ?>><a href="<?php print $item['url']; ?>"><?php print check_plain($item['label']); ?></a></li>
+          <li<?php print $item['class'] ? ' class="' . $item['class'] . '"' : '' ?>><a href="<?php print $item['url']; ?>" title="<?php print check_plain($item['label']); ?>"><?php print check_plain($item['label']); ?></a></li>
         <?php endforeach; ?>
       <?php else: ?>
         <li><?php print check_plain($item['label']); ?></li>
