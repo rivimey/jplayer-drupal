2e508a71fbbeff2c4548b86eb54a76bdbecca268
Jordan de Laune <jordan.delaune@htb.org.uk>
2011-03-10 11:04:46 +0000

    Changed package Added time formatting options

--- theme/jplayer.js
+++ theme/jplayer.js
@@ -16,6 +16,18 @@
           ready: function() {
             $(this).jPlayer("setMedia", playerSettings.files);
             
+            $.jPlayer.timeFormat.showHour = playerSettings.showHour;
+            $.jPlayer.timeFormat.showMin = playerSettings.showMin;
+            $.jPlayer.timeFormat.showSec = playerSettings.showSec;
+            
+            $.jPlayer.timeFormat.padHour = playerSettings.padHour;
+            $.jPlayer.timeFormat.padMin = playerSettings.padMin;
+            $.jPlayer.timeFormat.padSec = playerSettings.padSec;
+            
+            $.jPlayer.timeFormat.sepHour = playerSettings.sepHour;
+            $.jPlayer.timeFormat.sepMin = playerSettings.sepMin;
+            $.jPlayer.timeFormat.sepSec = playerSettings.sepSec;
+            
             // Add bindings
             $(player).bind($.jPlayer.event.play, function() {
               $(this).jPlayer("pauseOthers");
